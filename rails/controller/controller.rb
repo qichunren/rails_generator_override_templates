5ae286bec5632fce7ce4aaf462c91322c79acc7a
Qichunren 曹金华 <whyruby@gmail.com>
2012-01-12 13:26:05 +0800

    first commit

---
+++ rails/controller/controller.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+<% module_namespacing do -%>
+class <%= class_name %>Controller < ApplicationController
+<% actions.each do |action| -%>
+  def <%= action %>
+  end
+<%= "\n" unless action == actions.last -%>
+<% end -%>
+end
+<% end -%>
---
+++ rails/helper/helper.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+<% module_namespacing do -%>
+module <%= class_name %>Helper
+end
+<% end -%>
